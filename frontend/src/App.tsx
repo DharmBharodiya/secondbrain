29f6ef1bdc490517bcabeedd2a553558e2cb006f
DharmBharodiya <dharmbharodiya@gmail.com>
2026-03-24 23:34:49 +0530

    start - initalized the frontend for the app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1 className="w-full min-h-screen flex justify-center items-cente font-garamond text-5xl">
+      Hello, &thinsp;{" "}
+      <span className="font-garamond italic font-bold"> world</span>
+    </h1>
+  );
+}
+
+export default App;
